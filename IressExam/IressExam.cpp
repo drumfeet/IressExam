cfdc1027567b52cdfee0521f1fc91462bb8f3481
marssmarsshan <marssmarsshan@gmail.com>
2022-02-04 19:54:31 +0800

    handle user input

--- IressExam/IressExam.cpp
+++ IressExam/IressExam.cpp
@@ -1,20 +1,35 @@
-// IressExam.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Robot.h"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Input!\n";
+    bool exit_app = false;
+    while (!exit_app)
+    {
+      std::string str;
+      std::getline(std::cin, str);
+
+      std::string::size_type pos = str.find(" ");
+      std::string command = str.substr(0, pos);
+      std::string args = str.substr(pos + 1);
+
+      if (command.compare("EXIT") == 0)
+      {
+        exit_app = true;
+      }
+      else
+      {
+        std::vector<std::string> input = { command, args };
+      }
+    }
+
+
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
